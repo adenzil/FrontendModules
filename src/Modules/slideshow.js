f5fac79967bad0b51c5b7f62259d6059c3c5b4d3
alister <alistercabral@gmail.com>
2020-04-24 16:12:15 +0530

    add slides in html

--- src/Modules/slideshow.js
+++ src/Modules/slideshow.js
@@ -6,6 +6,7 @@ import '../Styles/style.scss';
 var slideshow = {
 	selector: "#slideshow",
 	$selector: $("#slideshow"),
+	$slides: $("#slideshow .slide"),
 	interval: 3000,
 	fadeTime: 1000,
 	currentSlide: 0,
@@ -14,43 +15,12 @@ var slideshow = {
 	intervalTimer: null,
 	toggleButtons: true,
 	dots: true,
-	content: [
-		{
-			"image": "http://farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg",
-			"text": "Flowers"
-		},
-		{
-			"image": "http://farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg",
-			"text": "Dog"
-		},
-		{
-			"text": "Default text"
-		}
-	]
-}
-
-slideshow.updateContent = (content) => {
-	slideshow.content = content;
-	return slideshow;
 }
 
 slideshow.generate = () => {
 
-	$.each(slideshow.content, (index, obj) => {
-		if(!$.isEmptyObject(obj)) {
-			var $div = $("<div>", { class:"slide", attr: {index} });
-			slideshow.$selector.append($div);
-		}
-		if(obj.image) {
-			$div.append($("<img>", {src: obj.image}));
-		}
-		if(obj.text) {
-			var $text = $("<div>", {text: obj.text}).css("text-align", "center");
-			$div.append($text);
-			if(obj.textPosition) {
-				$text.css("text-align", obj.textPosition);
-			}
-		}
+	slideshow.$slides.each((index, slide) => {
+		$(slide).attr('index', index).addClass("hide");
 	});
 
 	if(slideshow.toggleButtons) {
@@ -61,7 +31,7 @@ slideshow.generate = () => {
 	if(slideshow.dots) {
 		var $dots = $("<div>", {class: "dots"});
 		$("body").append($dots);
-		$.each(slideshow.content, (index, obj) => {
+		slideshow.$slides.each((index, obj) => {
 			$dots.append($("<span>", {class: "dot", attr: {index}}).bind('click', index, slideshow.goToSlide));
 		})
 	}
@@ -73,13 +43,13 @@ slideshow.generate = () => {
 
 slideshow.start = () => {
 
-	slideshow.$selector.find(".slide").hide();
+	slideshow.$slides.hide();
 
 	if(slideshow.autoChange) {
 		slideshow.intervalTimer = setInterval(function() {
 			var $activeSlide = slideshow.getSlide();
 			$activeSlide.fadeOut(slideshow.fadeTime);
-			slideshow.currentSlide = (slideshow.currentSlide + 1 < slideshow.content.length  ? slideshow.currentSlide+1 : 0);
+			slideshow.currentSlide = (slideshow.currentSlide + 1 < slideshow.$slides.length  ? slideshow.currentSlide+1 : 0);
 			slideshow.getSlide().fadeIn(slideshow.fadeTime);
 			slideshow.$selector.find(".dot").removeClass(".dot").find(`[index="${slideshow.currentSlide}"]`).addClass("active");
 		},  slideshow.interval);
@@ -110,12 +80,12 @@ slideshow.goToSlide = (event, slide) => {
 }
 
 slideshow.nextSlide = () => {
-	slideshow.currentSlide = slideshow.currentSlide+1 < slideshow.content.length  ? slideshow.currentSlide+1 : 0;
+	slideshow.currentSlide = slideshow.currentSlide+1 < slideshow.$slides.length  ? slideshow.currentSlide+1 : 0;
 	slideshow.goToSlide(null, slideshow.currentSlide);
 }
 
 slideshow.prevSlide = () => {
-	slideshow.currentSlide = slideshow.currentSlide == 0  ? slideshow.content.length-1 : slideshow.currentSlide-1;
+	slideshow.currentSlide = slideshow.currentSlide == 0  ? slideshow.$slides.length-1 : slideshow.currentSlide-1;
 	slideshow.goToSlide(null, slideshow.currentSlide);
 }
 
